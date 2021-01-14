5d40cf41de2191a6916dee7b01bc455ee65a0e1f
anord925 <alexnord925@gmail.com>
2021-01-14 11:12:08 -0700

    Removed the suggestion to add Mirage to PATH (I'm assuming that if they want to they'll have thought of that already)

--- SETUP.pl
+++ SETUP.pl
@@ -117,10 +117,7 @@ if (system($MirageLink)) { die "\n  Failed to create symbolic link to src/run_mi
 
 # Now the only thing the user REALLY needs to do is make
 # sure that spaln is on their PATH.
-print "\n\n  Setup completed successfully!";
-print "\n\n  To complete installation add mirage to your PATH\n";
-print "  (see README for assistance)\n\n";
-
+print "\n\n  Setup completed successfully!\n\n";
 
 # Happy mirage-ing!
 1;
