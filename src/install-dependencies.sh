e684905edbdfe57135b4920d312b93b99902e320
alexander-nord <alexnord925@gmail.com>
2022-08-12 17:14:14 -0600

    Added conditionals to check if we've already built specific dependencies before unpacking and building them unnecessarily

--- src/install-dependencies.sh
+++ src/install-dependencies.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env sh
 
+
 set -e
+
 
 TOTAL_NUM_TESTS=30
 GLOBAL_TEST_NUM=1
 LOCAL_TEST_NUM=1
+
 
 confirm_file_exists()
 {
@@ -15,6 +18,7 @@ confirm_file_exists()
 	exit 1
     fi
 }
+
 
 confirm_identical_files()
 {
@@ -30,6 +34,7 @@ confirm_identical_files()
     fi
 }
 
+
 report_test_start()
 {
     TEST_TYPE=$1
@@ -41,6 +46,116 @@ report_test_start()
     printf "$GLOBAL_TEST_NUM/$TOTAL_NUM_TESTS]: $TEST_TYPE test $LOCAL_TEST_NUM..."
     GLOBAL_TEST_NUM=$(($GLOBAL_TEST_NUM + 1))
     LOCAL_TEST_NUM=$(($LOCAL_TEST_NUM + 1))
+}
+
+
+# sindex testing
+check_sindex()
+{
+    FILE_NAME=$1
+
+    INPUT_FILE=$INPUTS_DIR/$FILE_NAME
+    OBSERVED_FILE=$INPUTS_DIR/$FILE_NAME.hsi
+    EXPECTED_FILE=$EXP_OUTPUTS_DIR/$FILE_NAME.hsi
+
+    if [ -e $OBSERVED_FILE ];
+    then
+	rm $OBSERVED_FILE
+    fi
+
+    report_test_start sindex
+    $SINDEX $INPUT_FILE
+    confirm_identical_files $OBSERVED_FILE $EXPECTED_FILE sindex
+    echo ' passed'
+}
+
+
+# sstat
+check_sstat()
+{
+    FILE_NAME=$1
+
+    INPUT_FILE=$INPUTS_DIR/$FILE_NAME
+    OBSERVED_FILE=$OBS_OUTPUTS_DIR/$FILE_NAME.sstat
+    EXPECTED_FILE=$EXP_OUTPUTS_DIR/$FILE_NAME.sstat
+
+    report_test_start sstat
+    $SSTAT $INPUT_FILE > $OBSERVED_FILE
+    confirm_identical_files $OBSERVED_FILE $EXPECTED_FILE sstat
+    echo ' passed'
+}
+
+
+# sfetch
+check_sfetch()
+{
+    RANGE_OPT=$1
+    SEQ_NAME=$2
+    IN_FILE_NAME=$3
+    OUT_FILE_NAME=$4
+
+    INPUT_FILE=$INPUTS_DIR/$IN_FILE_NAME
+    OBSERVED_FILE=$OBS_OUTPUTS_DIR/$OUT_FILE_NAME
+    EXPECTED_FILE=$EXP_OUTPUTS_DIR/$OUT_FILE_NAME
+
+    report_test_start sfetch
+    $SFETCH $RANGE_OPT $INPUT_FILE $SEQ_NAME > $OBSERVED_FILE
+    confirm_identical_files $OBSERVED_FILE $EXPECTED_FILE sfetch
+    echo ' passed'
+}
+
+
+check_spaln()
+{
+    AMINO_NAME=$1
+    DNA_NAME=$2
+    OUT_FILE_NAME=$3
+
+    AMINO_INPUT=$INPUTS_DIR/$AMINO_NAME
+    DNA_INPUT=$EXP_OUTPUTS_DIR/$DNA_NAME
+    OBSERVED_FILE=$OBS_OUTPUTS_DIR/$OUT_FILE_NAME
+    EXPECTED_FILE=$EXP_OUTPUTS_DIR/$OUT_FILE_NAME
+
+    report_test_start spaln
+    $SPALN -Q3 -O1 -S1 -ya3 -yz4 -yy4 $DNA_INPUT $AMINO_INPUT 1>$OBSERVED_FILE
+    confirm_identical_files $OBSERVED_FILE $EXPECTED_FILE spaln
+    echo ' passed'
+}
+
+
+check_tblastn()
+{
+    DNA_NAME=$1
+    AMINO_NAME=$2
+    OUT_FILE_NAME=$3
+
+    DNA_INPUT=$INPUTS_DIR/$DNA_NAME
+    AMINO_INPUT=$INPUTS_DIR/$AMINO_NAME
+    OBSERVED_FILE=$OBS_OUTPUTS_DIR/$OUT_FILE_NAME
+    EXPECTED_FILE=$EXP_OUTPUTS_DIR/$OUT_FILE_NAME
+
+    report_test_start tblastn
+    $TBLASTN -outfmt 6 -subject $DNA_INPUT -query $AMINO_INPUT -out $OBSERVED_FILE 1>/dev/null
+    confirm_identical_files $OBSERVED_FILE $EXPECTED_FILE tblastn
+    echo ' passed'
+}
+
+
+check_blat()
+{
+    DNA_NAME=$1
+    AMINO_NAME=$2
+    OUT_FILE_NAME=$3
+
+    DNA_INPUT=$INPUTS_DIR/$DNA_NAME
+    AMINO_INPUT=$INPUTS_DIR/$AMINO_NAME
+    OBSERVED_FILE=$OBS_OUTPUTS_DIR/$OUT_FILE_NAME
+    EXPECTED_FILE=$EXP_OUTPUTS_DIR/$OUT_FILE_NAME
+
+    report_test_start blat
+    $BLAT -tileSize=7 -minIdentity=90 -maxIntron=1  -t=dnax -q=prot -out=blast8 -minScore=40 1>/dev/null $DNA_INPUT $AMINO_INPUT $OBSERVED_FILE
+    confirm_identical_files $OBSERVED_FILE $EXPECTED_FILE blat
+    echo ' passed'
 }
 
 HSI_BASE=hsi
@@ -117,192 +232,108 @@ confirm_file_exists $SPECIES_GUIDE
 
 
 # 1 HSI
-tar -xf $HSI_TAR -C $DEPS_DIR
-cd $HSI_DIR && cmake . && make && cd -
+if [[ -d $BUILD_DIR/$HSI_BASE ]]
+then
+    echo " HSI already built (remove $BUILD_DIR/$HSI_BASE to remake)"
+else
+    tar -xf $HSI_TAR -C $DEPS_DIR
+    cd $HSI_DIR && cmake . && make && cd -
 
-SINDEX=$HSI_DIR/build/sindex
-SSTAT=$HSI_DIR/build/sstat
-SFETCH=$HSI_DIR/build/sfetch
+    SINDEX=$HSI_DIR/build/sindex
+    SSTAT=$HSI_DIR/build/sstat
+    SFETCH=$HSI_DIR/build/sfetch
 
+    LOCAL_TEST_NUM=1
+    check_sindex $AA1_NAME    # test 1
+    check_sindex $AA2_NAME    # test 2
+    check_sindex $AA3_NAME    # test 3
+    check_sindex $AA123_NAME  # test 4
+    check_sindex $DNA1_NAME   # test 5
+    check_sindex $DNA2_NAME   # test 6
+    check_sindex $DNA3_NAME   # test 7
 
+    LOCAL_TEST_NUM=1
+    check_sstat $AA1_NAME    # test  8
+    check_sstat $AA2_NAME    # test  9
+    check_sstat $AA3_NAME    # test 10
+    check_sstat $AA123_NAME  # test 11
+    check_sstat $DNA1_NAME   # test 12
+    check_sstat $DNA2_NAME   # test 13
+    check_sstat $DNA3_NAME   # test 14
 
-# sindex
-check_sindex()
-{
-    FILE_NAME=$1
+    SP1_GENE2_DNA_NAME=sp1.g2.dna.fa
+    SP1_GENE3_DNA_NAME=sp1.g3.dna.fa
+    SP2_GENE2_DNA_NAME=sp2.g2.dna.fa
+    SP2_GENE3_DNA_NAME=sp2.g3.dna.fa
+    SP3_GENE23_DNA_NAME=sp3.g23.dna.fa
 
-    INPUT_FILE=$INPUTS_DIR/$FILE_NAME
-    OBSERVED_FILE=$INPUTS_DIR/$FILE_NAME.hsi
-    EXPECTED_FILE=$EXP_OUTPUTS_DIR/$FILE_NAME.hsi
+    LOCAL_TEST_NUM=1
+    check_sfetch '-range 16000..38000' chr1 $DNA1_NAME $SP1_GENE2_DNA_NAME   # test 15
+    check_sfetch '-range 1000..27000'  chr2 $DNA1_NAME $SP1_GENE3_DNA_NAME   # test 16
+    check_sfetch '-range 13000..38000' chr1 $DNA2_NAME $SP2_GENE2_DNA_NAME   # test 17
+    check_sfetch '-range 24000..1000'  chr2 $DNA2_NAME $SP2_GENE3_DNA_NAME   # test 18
+    check_sfetch '-range 13000..64500' chr1 $DNA3_NAME $SP3_GENE23_DNA_NAME  # test 19
 
-    if [ -e $OBSERVED_FILE ];
-    then
-	rm $OBSERVED_FILE
-    fi
-
-    report_test_start sindex
-    $SINDEX $INPUT_FILE
-    confirm_identical_files $OBSERVED_FILE $EXPECTED_FILE sindex
-    echo ' passed'
-}
-LOCAL_TEST_NUM=1
-check_sindex $AA1_NAME    # test 1
-check_sindex $AA2_NAME    # test 2
-check_sindex $AA3_NAME    # test 3
-check_sindex $AA123_NAME  # test 4
-check_sindex $DNA1_NAME   # test 5
-check_sindex $DNA2_NAME   # test 6
-check_sindex $DNA3_NAME   # test 7
-
-
-
-# sstat
-check_sstat()
-{
-    FILE_NAME=$1
-
-    INPUT_FILE=$INPUTS_DIR/$FILE_NAME
-    OBSERVED_FILE=$OBS_OUTPUTS_DIR/$FILE_NAME.sstat
-    EXPECTED_FILE=$EXP_OUTPUTS_DIR/$FILE_NAME.sstat
-
-    report_test_start sstat
-    $SSTAT $INPUT_FILE > $OBSERVED_FILE
-    confirm_identical_files $OBSERVED_FILE $EXPECTED_FILE sstat
-    echo ' passed'
-}
-LOCAL_TEST_NUM=1
-check_sstat $AA1_NAME    # test  8
-check_sstat $AA2_NAME    # test  9
-check_sstat $AA3_NAME    # test 10
-check_sstat $AA123_NAME  # test 11
-check_sstat $DNA1_NAME   # test 12
-check_sstat $DNA2_NAME   # test 13
-check_sstat $DNA3_NAME   # test 14
-
-
-
-# sfetch
-check_sfetch()
-{
-    RANGE_OPT=$1
-    SEQ_NAME=$2
-    IN_FILE_NAME=$3
-    OUT_FILE_NAME=$4
-
-    INPUT_FILE=$INPUTS_DIR/$IN_FILE_NAME
-    OBSERVED_FILE=$OBS_OUTPUTS_DIR/$OUT_FILE_NAME
-    EXPECTED_FILE=$EXP_OUTPUTS_DIR/$OUT_FILE_NAME
-
-    report_test_start sfetch
-    $SFETCH $RANGE_OPT $INPUT_FILE $SEQ_NAME > $OBSERVED_FILE
-    confirm_identical_files $OBSERVED_FILE $EXPECTED_FILE sfetch
-    echo ' passed'
-}
-SP1_GENE2_DNA_NAME=sp1.g2.dna.fa
-SP1_GENE3_DNA_NAME=sp1.g3.dna.fa
-SP2_GENE2_DNA_NAME=sp2.g2.dna.fa
-SP2_GENE3_DNA_NAME=sp2.g3.dna.fa
-SP3_GENE23_DNA_NAME=sp3.g23.dna.fa
-LOCAL_TEST_NUM=1
-check_sfetch '-range 16000..38000' chr1 $DNA1_NAME $SP1_GENE2_DNA_NAME   # test 15
-check_sfetch '-range 1000..27000'  chr2 $DNA1_NAME $SP1_GENE3_DNA_NAME   # test 16
-check_sfetch '-range 13000..38000' chr1 $DNA2_NAME $SP2_GENE2_DNA_NAME   # test 17
-check_sfetch '-range 24000..1000'  chr2 $DNA2_NAME $SP2_GENE3_DNA_NAME   # test 18
-check_sfetch '-range 13000..64500' chr1 $DNA3_NAME $SP3_GENE23_DNA_NAME  # test 19
-
-mv -f $HSI_DIR $BUILD_DIR/$HSI_BASE
-
+    mv -f $HSI_DIR $BUILD_DIR/$HSI_BASE
+fi
 
 
 # SPALN
-tar -xf $SPALN_TAR -C $DEPS_DIR
-cd $SPALN_DIR/src && ./configure && make && cd -
-SPALN=$SPALN_DIR/src/spaln
-check_spaln()
-{
-    AMINO_NAME=$1
-    DNA_NAME=$2
-    OUT_FILE_NAME=$3
+if [[ -d $BUILD_DIR/$SPALN_BASE ]]
+then
+    echo " Spaln already built (remove $BUILD_DIR/$SPALN_BASE to remake)"
+else
+    tar -xf $SPALN_TAR -C $DEPS_DIR
+    cd $SPALN_DIR/src && ./configure && make && cd -
+    SPALN=$SPALN_DIR/src/spaln
 
-    AMINO_INPUT=$INPUTS_DIR/$AMINO_NAME
-    DNA_INPUT=$EXP_OUTPUTS_DIR/$DNA_NAME
-    OBSERVED_FILE=$OBS_OUTPUTS_DIR/$OUT_FILE_NAME
-    EXPECTED_FILE=$EXP_OUTPUTS_DIR/$OUT_FILE_NAME
-
-    report_test_start spaln
-    $SPALN -Q3 -O1 -S1 -ya3 -yz4 -yy4 $DNA_INPUT $AMINO_INPUT 1>$OBSERVED_FILE
-    confirm_identical_files $OBSERVED_FILE $EXPECTED_FILE spaln
-    echo ' passed'
-}
-LOCAL_TEST_NUM=1
-echo '* The following 5 spaln tests may take a minute...'
-check_spaln $AA1_NAME $SP1_GENE2_DNA_NAME sp1.g2.spaln.out    # test 20
-check_spaln $AA1_NAME $SP1_GENE3_DNA_NAME sp1.g3.spaln.out    # test 21
-check_spaln $AA2_NAME $SP2_GENE2_DNA_NAME sp2.g2.spaln.out    # test 22
-check_spaln $AA2_NAME $SP2_GENE3_DNA_NAME sp2.g3.spaln.out    # test 23
-check_spaln $AA3_NAME $SP3_GENE23_DNA_NAME sp3.g23.spaln.out  # test 24
-
-mv -f $SPALN_DIR $BUILD_DIR/$SPALN_BASE
-
+    LOCAL_TEST_NUM=1
+    echo '* The following 5 Spaln tests may take a minute...'
+    check_spaln $AA1_NAME $SP1_GENE2_DNA_NAME sp1.g2.spaln.out    # test 20
+    check_spaln $AA1_NAME $SP1_GENE3_DNA_NAME sp1.g3.spaln.out    # test 21
+    check_spaln $AA2_NAME $SP2_GENE2_DNA_NAME sp2.g2.spaln.out    # test 22
+    check_spaln $AA2_NAME $SP2_GENE3_DNA_NAME sp2.g3.spaln.out    # test 23
+    check_spaln $AA3_NAME $SP3_GENE23_DNA_NAME sp3.g23.spaln.out  # test 24
+    
+    mv -f $SPALN_DIR $BUILD_DIR/$SPALN_BASE
+fi
 
 
 # TBLASTN
-tar -xf $TBLASTN_TAR -C $DEPS_DIR
-TBLASTN=$TBLASTN_DIR/tblastn.linux.x86_64
-check_tblastn()
-{
-    DNA_NAME=$1
-    AMINO_NAME=$2
-    OUT_FILE_NAME=$3
+if [[ -d $BUILD_DIR/$TBLASTN_BASE ]]
+then
+    echo " Tblastn already built (remove $BUILD_DIR/$TBLASTN to remake)"
+else
+    tar -xf $TBLASTN_TAR -C $DEPS_DIR
+    TBLASTN=$TBLASTN_DIR/tblastn.linux.x86_64
+    
+    LOCAL_TEST_NUM=1
+    check_tblastn $DNA1_NAME $AA1_NAME sp1.tblastn.out  # test 28
+    check_tblastn $DNA2_NAME $AA2_NAME sp2.tblastn.out  # test 29
+    check_tblastn $DNA3_NAME $AA3_NAME sp3.tblastn.out  # test 30
 
-    DNA_INPUT=$INPUTS_DIR/$DNA_NAME
-    AMINO_INPUT=$INPUTS_DIR/$AMINO_NAME
-    OBSERVED_FILE=$OBS_OUTPUTS_DIR/$OUT_FILE_NAME
-    EXPECTED_FILE=$EXP_OUTPUTS_DIR/$OUT_FILE_NAME
-
-    report_test_start tblastn
-    $TBLASTN -outfmt 6 -subject $DNA_INPUT -query $AMINO_INPUT -out $OBSERVED_FILE 1>/dev/null
-    confirm_identical_files $OBSERVED_FILE $EXPECTED_FILE tblastn
-    echo ' passed'
-}
-LOCAL_TEST_NUM=1
-check_tblastn $DNA1_NAME $AA1_NAME sp1.tblastn.out  # test 28
-check_tblastn $DNA2_NAME $AA2_NAME sp2.tblastn.out  # test 29
-check_tblastn $DNA3_NAME $AA3_NAME sp3.tblastn.out  # test 30
-
-mv -f $TBLASTN_DIR $BUILD_DIR/$TBLASTN_BASE
-
+    mv -f $TBLASTN_DIR $BUILD_DIR/$TBLASTN_BASE
+fi
 
 
 # BLAT
-tar -xzf $BLAT_TAR -C $DEPS_DIR
-cd $BLAT_DIR/lib && make && cd -
-cd $BLAT_DIR/jkOwnLib && make && cd -
-cd $BLAT_DIR/blat && mkdir -p ../bin && BINDIR=$PWD/../bin make && cd -
-BLAT=$BLAT_DIR/bin/blat
-check_blat()
-{
-    DNA_NAME=$1
-    AMINO_NAME=$2
-    OUT_FILE_NAME=$3
+if [[ -d $BUILD_DIR/$BLAT_BASE ]]
+then
+    echo " Blat already built (remove $BUILD_DIR/$BLAT_BASE to remake)"
+else
+    tar -xzf $BLAT_TAR -C $DEPS_DIR
+    cd $BLAT_DIR/lib && make && cd -
+    cd $BLAT_DIR/jkOwnLib && make && cd -
+    cd $BLAT_DIR/blat && mkdir -p ../bin && BINDIR=$PWD/../bin make && cd -
+    BLAT=$BLAT_DIR/bin/blat
+    
+    LOCAL_TEST_NUM=1
+    check_blat $DNA1_NAME $AA1_NAME sp1.blat.out  # test 25
+    check_blat $DNA2_NAME $AA2_NAME sp2.blat.out  # test 26
+    check_blat $DNA3_NAME $AA3_NAME sp3.blat.out  # test 27
 
-    DNA_INPUT=$INPUTS_DIR/$DNA_NAME
-    AMINO_INPUT=$INPUTS_DIR/$AMINO_NAME
-    OBSERVED_FILE=$OBS_OUTPUTS_DIR/$OUT_FILE_NAME
-    EXPECTED_FILE=$EXP_OUTPUTS_DIR/$OUT_FILE_NAME
-
-    report_test_start blat
-    $BLAT -tileSize=7 -minIdentity=90 -maxIntron=1  -t=dnax -q=prot -out=blast8 -minScore=40 1>/dev/null $DNA_INPUT $AMINO_INPUT $OBSERVED_FILE
-    confirm_identical_files $OBSERVED_FILE $EXPECTED_FILE blat
-    echo ' passed'
-}
-LOCAL_TEST_NUM=1
-check_blat $DNA1_NAME $AA1_NAME sp1.blat.out  # test 25
-check_blat $DNA2_NAME $AA2_NAME sp2.blat.out  # test 26
-check_blat $DNA3_NAME $AA3_NAME sp3.blat.out  # test 27
-
-mv -f $BLAT_DIR $BUILD_DIR/$BLAT_BASE
-
+    mv -f $BLAT_DIR $BUILD_DIR/$BLAT_BASE
+fi
 
 
 # Cleanup
